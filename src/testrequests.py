07a90763d18d086a9eff03d7690f76c85a6aca01
roboticsteam <jjohnson18@gssm.k12.sc.us>
2017-04-06 23:38:09 -0400

    Ran full iteration of hartsvillesc extraction and switched to use hartsvillesc-test.json

--- src/testrequests.py
+++ src/testrequests.py
@@ -10,27 +10,123 @@
 from lxml import html
 import requests
 import json
+import string
+import itertools
 
 from datetime import datetime
 import dateutil.parser as dateutil #TODO actually use this
 
+import os, sys
+from os.path import join as pathjoin, abspath, dirname
+
+
+def project_root():
+    '''
+    Gets absolute path of project root
+
+    :return: Returns absolute path string for project root directory
+    '''
+    return pathjoin(dirname(abspath(sys.argv[0])), "..") #gets root of
+
+def partition(pred, dictionary):
+    '''
+    Use a predicate to partition dictionary entries into false entries and true entries
+
+    :param pred: Predicate lambda applied on a tuple (key,value) which will return True or False
+    :param iterable: Dictionary to partition
+
+    Stolen itertools-recipe
+    '''
+    # partition(is_odd, range(10)) --> 0 2 4 6 8   and  1 3 5 7 9
+    t1, t2 = itertools.tee(dictionary.items())
+    first = dict(itertools.filterfalse(pred, t1))
+    print(first)
+    second = dict(filter(pred, t2))
+    print(second)
+    return first, second
+
+def extract_from_template(json_filename):
+    '''
+    Reads json template file then extracts data according to its rules and writes it where specified
+    '''
+    template = get_template(pathjoin(project_root(), "templates", json_filename)) #filename in templates directory
+    print("Yes")
+    extract(template)
+
+def get_template(json_filename):
+    '''
+    Reads from a file and gets the json template object
+    '''
+    with open(json_filename) as fp:
+        return json.load(fp)
+
+def partition_rules(rules_dict):
+    '''
+    Get annotations (modifiers) and processes (things to process) from rules_dict
+
+    :param rules_dict: Dictionary with keys starting with @ (annotations) and letters (processes)
+    :return: Returns lists of (annotations, processes)
+    '''
+    others = dict(filter(lambda key: len(key[0]) > 0, rules_dict.items())) #gets everything that has a length
+    others, annotations = partition(lambda key: key[0] != '@', others) #gets all keys beginning with @
+    others, processes = partition(lambda key: key[0] in string.ascii_letters, others) #get all keys beginning with a letter
+    return annotations, processes
 
 def extract(rules_dict):
     '''
-    Extracts an entire set of data based on a
+    Extracts an entire set of data based on a rules dictionary
     '''
-    pass #TODO
+    annotations, processes = partition_rules(rules_dict)
 
-def extract_nodes(doc_root, rules_dict, variables={}):
+    output_dir = pathjoin(project_root(), (annotations.get("@output_dir") or ".")) #get output_dir or use blank relative path #TODO possibly need to use . for relative location
+
+    print(annotations)
+    print(processes)
+
+    for process_key in processes:
+        #loop through each process and complete it
+        #TODO do this with threading/multiprocessing since they are all separate
+        process_rules = rules_dict[process_key]
+        process_output = pathjoin(output_dir, process_key + ".json") #save to process_key with .json extension
+        extract_process(process_rules, process_output) #extract from the individual process
+
+def get_variables(annotations):
     '''
-    Extracts a complete node from a site
+    Gets all variables over which the process iterates (start with @var and @list)
+
+    :param annotations: Dictionary with all keys beginning with @
+    :return: Returns two dictionaries (one with vars and one with lists mapped by name)
+    '''
+    variables = {}
+    lists = {}
+
+    for annotation in annotations.items():
+        if annotation[0].startswith("@var:"):
+            variables[annotation[0].replace("@var:")] = annotation[1]
+        if annotation[0].startswith("@list:"):
+            lists[annotations[0].replace("@list:")] = annotation[1]
+    return variables, lists
+
+def extract_process(rules_dict, output_file, variables={}):
+    annotations, misc = partition_rules(rules_dict) #get annotations and misc (there shouldn't be anything without annotation)
+
+    variables, lists = get_variables(annotations)
+
+    print(variables)
+    print(lists)
+
+    pass
+
+def extract_nodes(doc_root, rules_dict, output_file, variables={}):
+    '''
+    Extracts a complete node from a document via a ruled dictionary
 
     :param doc_root: document root element (created by lxml.html.fromstring())
     :rules_dict: dictionary with node rules
     '''
     pass
 
-def inner_extract(doc_root, node_root, node_details, output_file=None, variables={}):
+def inner_extract(doc_root, node_root, node_details, output_file, variables={}):
     '''
     Extracts data from a site according to rules
 
@@ -52,7 +148,7 @@ if __name__ == "__main__":
     event_list = [] #list of events to be put together (each event will be dictionary)
 
     #TODO TODO TODO remove this debugging part
-    while not done: # and pg_num < 3:
+    while not done and pg_num < 3 and False:
         #Loop through all pages of the calendar
 
         page = requests.get('https://www.hartsvillesc.gov/calendar/?action=tribe_photo&tribe_paged=%d&tribe_event_display=photo' % pg_num)
@@ -93,11 +189,10 @@ if __name__ == "__main__":
                 if len(stops) > 0:
                     stopstr = stops[0].strip()
                     try:
+                        start_time_str = new_event.get('start')
+                        start_time = datetime.strptime(new_event['start'], '%m/%d/%Y %H:%M:%S') if start_time_str != None else None
 
-                        if 'start' in new_event.keys():
-                            try_datetime = dateutil.parse(stopstr, fuzzy=True, default=datetime.strptime(new_event['start'], '%m/%d/%Y %H:%M:%S'))
-                        else:
-                            try_datetime = dateutil.parse(stopstr, fuzzy=True)
+                        try_datetime = dateutil.parse(stopstr, fuzzy=True, default=start_time)
 
                         output_datetime = datetime.strftime(try_datetime, '%m/%d/%Y %H:%M:%S')
 
@@ -116,6 +211,8 @@ if __name__ == "__main__":
 
         pg_num += 1
 
-    json_text = json.dumps(event_list, indent=4)
-    with open("results/hartsvillesc-test.json", "w+") as fp:
-        fp.write(json_text)
+    extract_from_template("hartsvillesc.json")
+
+    # json_text = json.dumps(event_list, indent=4)
+    # with open(os.path.join(project_root(), "results", "hartsvillesc-test2.json"), "w+") as fp:
+    #     fp.write(json_text)
