37bf090f5b71582ca0768b00cd952e18e604eecd
Hunter Damron <hdamron1594@yahoo.com>
2017-04-09 19:13:27 -0400

    Gets information but does not output in goof format yet

--- src/testrequests.py
+++ src/testrequests.py
@@ -28,16 +28,19 @@ def project_root():
     '''
     return pathjoin(dirname(abspath(sys.argv[0])), "..") #gets root of
 
-def extract_from_template(json_filename):
+def extract_from_template(json_filename, debug=False):
     '''
     Reads json template file then extracts data according to its rules and writes it where specified
     '''
     template = get_template(pathjoin(project_root(), "templates", json_filename)) #filename in templates directory
-    extract(template)
+    extract(template, debug=debug)
 
 def get_template(json_filename):
     '''
     Reads from a file and gets the json template object
+
+    :param json_filename: filename to open json from
+    :return: returns json object from the file
     '''
     with open(json_filename) as fp:
         return json.load(fp)
@@ -55,20 +58,22 @@ def partition_rules(rules_dict):
 
     return annotations, processes
 
-def extract(rules_dict):
+def extract(rules_dict, debug=False):
     '''
     Extracts an entire set of data based on a rules dictionary
     '''
     annotations, processes = partition_rules(rules_dict)
 
-    output_dir = pathjoin(project_root(), (annotations.get("@output_dir") or ".")) #get output_dir or use blank relative path #TODO possibly need to use . for relative location
+    output_dir = pathjoin(project_root(), "results", (annotations.get("@output_dir") or ".")) #get output_dir or use blank relative path #TODO possibly need to use . for relative location
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
     for process_key in processes:
         #loop through each process and complete it
-        #TODO do this with threading/multiprocessing since they are all separate
+        #TODO do this with threading/multiprocessing since they are all separate  #TODO maybe not because of end condition (see later)
         process_rules = rules_dict[process_key]
         process_output = pathjoin(output_dir, process_key + ".json") #save to process_key with .json extension
-        extract_process(process_rules, process_output) #extract from the individual process
+        extract_process(process_rules, process_output, debug=debug) #extract from the individual process
 
 def get_variables(annotations):
     '''
@@ -88,9 +93,13 @@ def get_variables(annotations):
 
     return variables, lists
 
-def extract_process(rules_dict, output_file, variables={}):
+def extract_process(rules_dict, output_file, variables={}, debug=False):
     '''
-    #TODO
+    Extracts from a single process in the template and outputs it to a file
+
+    :param rules_dict: Inner dictionary from the specific process
+    :param output_file: Filename to output results to in json format
+    :return: returns True if the extraction was successful else False
     '''
     annotations, misc = partition_rules(rules_dict) #get annotations and misc (there shouldn't be anything without annotation)
 
@@ -101,42 +110,102 @@ def extract_process(rules_dict, output_file, variables={}):
     url = annotations.get("@url")
     if url == None: return False #url is required for process
 
+    results = []
+
     done = False
     while not done:
         for var in variables.items():
-            url = url.replace("{$%s}" % var[0], str(var[1])) #replace any instance of variables in url
-            extract_nodes_from_url(url, rules_dict, output_file)
+            temp_url = url.replace("{$%s}" % var[0], str(var[1])) #replace any instance of variables in url
 
-        done = True #TODO add done condition of all variables exhausted or there are no variables
+            single_result = extract_node_from_url(temp_url, rules_dict, debug=debug) #TODO we can't use threading for this because the return must be used to determine if it changed anything
 
-    return True
+        #nothing was found at the specified url
+        if len(single_result) <= 0:
+            done = True
 
-def extract_nodes_from_url(url, rules_dict, output_file, variables={}):
+        #incremement numeric variable
+        for var_key in new_variables.keys():
+            variables[var_key] += 1
+
+        #increment list variables and determine if it is the end of the list
+        for list_key in new_lists.keys():
+            cur_val = variables[list_key]
+            cur_index = new_lists[list_key].index(cur_val)
+            if cur_index == len(new_lists[list_key]) - 1:
+                done = True #this is the last element in the list
+            else:
+                variables[list_key] = new_lists[list_key][cur_index+1]
+
+        #add single result to the list
+        # for node_name in single_result.keys():
+        #     if node_name not in results.keys():
+        #         results[node_name] = {}
+        #     results[node_name].extend(single_result)
+        results.append(single_result)
+
+    #write the results to file
+    json_text = json.dumps(results, indent=4) #later we can take out indent which makes it readable
+    with open(output_file, "w+") as fp:
+        fp.write(json_text)
+
+    return True #if nothing went wrong, it's a success
+
+def extract_node_from_url(url, rules_dict, debug=False):
     '''
-    Extracts a complete node from a document via ruled dictionary from url (allows use of threading)
+    Extracts a complete node from a document via ruled dictionary from url
 
     :param url: String url to pull from
     :rules_dict: dictionary with node rules
     :param output_file: filename to write to (absolute)
     :param variables: dictionary of variables to use
-    :return: Returns success rate boolean
+    :return: Returns True if the query was nonempty else False
     '''
-    print(url) #TODO remove
-    pass #TODO
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
+    return extract_node(tree, rules_dict, debug=debug)
 
-def extract_nodes(doc_root, rules_dict, output_file, variables={}):
+def extract_node(doc_root, rules_dict, debug=False):
     '''
     Extracts a complete node from a document via a ruled dictionary
 
     :param doc_root: document root element (created by lxml.html.fromstring())
     :rules_dict: dictionary with node rules
-    :param output_file: filename to write to (absolute)
-    :param variables: dictionary of variables to use
     :return: Returns success rate boolean
     '''
-    pass
+    annotations, elements = partition_rules(rules_dict) #elements contain only xpath to pull from
 
-def inner_extract(doc_root, node_root, node_details, output_file, variables={}):
+    node_xpath = rules_dict.get("@xpath") or "." #gets xpath of node or uses relative location else
+
+    nodes = {} #nodes dicionary contains each node
+    follows = {} #xpath links to follow for more detailed information
+    for annotation in annotations.items():
+        if annotation[0].startswith("@node:"):
+            nodes[annotation[0].replace("@node:", "")] = annotation[1]
+        #TODO find follow nodes
+
+    node_roots = doc_root.xpath(node_xpath) #gets all nodes
+    node_results = []
+
+    #get all non-nodes from the node
+    for node_root in node_roots:
+        output = {} #single node output will be extended with each non-annotation
+
+        for name, element_xpath in elements.items():
+            results = node_root.xpath(element_xpath)
+            if len(results) > 0:
+                output[name] = results[0].strip()
+
+        #TODO apply follow paths to pull from other sites
+
+        for node_name, node_rules in nodes.items():
+            output[node_name] = extract_node(node_root, node_rules, debug=debug)
+
+        node_results.append(output) #add this node instance to the total list of results
+
+    return node_results
+
+#TODO this may be depricated because it is an unnecessary level of function
+def inner_extract(doc_root, node_root, node_details, output_file, variables={}, debug=False):
     '''
     Extracts data from a site according to rules
 
