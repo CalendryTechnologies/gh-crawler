396107708aa1997dd996e74da9214b83493f9672
hdamron1594 <hdamron1594@hdamron-ubuntu>
2017-04-16 22:33:02 -0400

    Works for hartsvillesc.gov but needs some updates for generality.

--- src/testrequests.py
+++ src/testrequests.py
@@ -13,9 +13,6 @@ import json
 import string
 import threading
 
-from datetime import datetime
-import dateutil.parser as dateutil #TODO actually use this
-
 import os, sys
 from os.path import join as pathjoin, abspath, dirname
 
@@ -28,7 +25,7 @@ def project_root():
 
     :return: Returns absolute path string for project root directory
     '''
-    return pathjoin(dirname(abspath(sys.argv[0])), "..") #gets root of
+    return pathjoin(dirname(abspath(sys.argv[0])), "..") #gets root of project (moving out of src)
 
 def extract_from_template(json_filename, debug=False):
     '''
@@ -149,7 +146,7 @@ def extract_process(rules_dict, output_file, variables={}, debug=False):
             results[node_name].extend(single_result[node_name]) #extend the node value list
 
     #write the results to file
-    json_text = json.dumps(results, indent=4) #later we can take out indent which makes it readable
+    json_text = json.dumps(results, indent=4, ensure_ascii=False) #later we can take out indent which makes it readable
     with open(output_file, "w+") as fp:
         fp.write(json_text)
 
@@ -232,102 +229,6 @@ def extract_node(doc_root, rules_dict, debug=False):
 
     return node_results
 
-def filter_time(node):
-    '''
-    Parses 'startstr' and 'stopstr' to add 'start' and 'stop'
-    
-    :param node: dictionary to be modified (modified in place)
-    '''
-    startstr = node.get('startstr')
-    stopstr = node.get('stopstr')
-    start_datetime = None
-    if startstr:
-        #the node has a start string
-        start_datetime = dateutil.parse(startstr, fuzzy=True)
-        new_startstr = datetime.strftime(start_datetime, '%m/%d/%Y %H:%M:%S')
-        node['start'] = new_startstr
-    if stopstr:
-        stop_datetime = dateutil.parse(stopstr, fuzzy=True, default=start_datetime)
-        new_stopstr = datetime.strftime(stop_datetime, '%m/%d/%Y %H:%M:%S')
-        node['stop'] = new_stopstr
-
-filter_dict = {
-    'time' : filter_time
-}
-
 if __name__ == "__main__":
-    pg_num = 1
-    done = False #have we finished looping through pages
-    datetime_parse_fmt = "%B %-d @ %I:%M %p" #format string for getting datetime object
-    datetime_fmt = "%m/%d/%Y %H:%M:%S" #format datetime should be in for standardization
-    event_list = [] #list of events to be put together (each event will be dictionary)
-
-    #TODO TODO TODO remove this debugging part
-    while not done and pg_num < 3 and False:
-        #Loop through all pages of the calendar
-
-        page = requests.get('https://www.hartsvillesc.gov/calendar/?action=tribe_photo&tribe_paged=%d&tribe_event_display=photo' % pg_num)
-        tree = html.fromstring(page.content)
-
-        events = tree.xpath("//div[contains(@class,'type-tribe_events')]")
-        notices = tree.xpath("//div[contains(@class,'tribe-events-notices')]/ul/li/text()")
-
-        done = False
-
-        if len(events) == 0:
-            done = True
-        else:
-            for event in events:
-                titles = event.xpath(".//h2[contains(@class,'tribe-events-list-event-title')]/a/text()")
-                starts = event.xpath(".//span[contains(@class,'tribe-event-date-start')]/text()")
-                stops = event.xpath(".//span[contains(@class,'tribe-event-time')]/text()")
-                descriptions = event.xpath(".//div[contains(@class,'tribe-events-content')]/p/text()")
-                links = event.xpath(".//a[contains(@class,'tribe-event-url')]/@href")
-                #TODO use link to get more information and possibly use lxml function to replace relative urls to help following
-
-                new_event = {}
-                if len(titles) <= 0:
-                    break #All items must have a title
-                else:
-                    new_event["title"] = titles[0].strip()
-
-                if len(starts) > 0:
-                    startstr = starts[0].strip()
-                    try:
-                        try_datetime = dateutil.parse(startstr, fuzzy=True)
-                        output_datetime = datetime.strftime(try_datetime, '%m/%d/%Y %H:%M:%S')
-                        new_event["start"] = output_datetime
-                    except: #TODO don't just exept everything
-                        print("Unable to parse start string '%s'" % startstr)
-                    new_event["startstr"] = startstr
-
-                if len(stops) > 0:
-                    stopstr = stops[0].strip()
-                    try:
-                        start_time_str = new_event.get('start')
-                        start_time = datetime.strptime(new_event['start'], '%m/%d/%Y %H:%M:%S') if start_time_str != None else None
-
-                        try_datetime = dateutil.parse(stopstr, fuzzy=True, default=start_time)
-
-                        output_datetime = datetime.strftime(try_datetime, '%m/%d/%Y %H:%M:%S')
-
-                        new_event["stop"] = output_datetime
-                    except: #TODO don't just exept everything
-                        print("Unable to parse stop string '%s'" % stopstr)
-                    new_event["stopstr"] = stopstr
-
-                if len(descriptions) > 0:
-                    new_event["description"] = descriptions[0].strip()
-
-                if len(links) > 0:
-                    new_event["link"] = links[0].strip()
-
-                event_list.append(new_event)
-
-        pg_num += 1
-
     extract_from_template("hartsvillesc.json")
 
-    # json_text = json.dumps(event_list, indent=4)
-    # with open(os.path.join(project_root(), "results", "hartsvillesc-test2.json"), "w+") as fp:
-    #     fp.write(json_text)
